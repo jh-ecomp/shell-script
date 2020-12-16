5cd95c2b458bd287a744f08e2a1701061f2f435d
Joao_Henrique_Trixlog <joaohenrique@trixlog.com>
2020-12-15 21:08:09 -0300

    corrigindo para uma só linha

--- exercicios/exercicio03/saudacao.sh
+++ exercicios/exercicio03/saudacao.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 # Correção: OK
-echo "Olá $(whoami)" | tee -a saudacao.log
-echo "Hoje é dia $(date +"%d"), do mês $(date +"%m") do ano de $(date +"%Y")" | tee -a saudacao.log
+echo -e "Olá $(whoami)\nHoje é dia $(date +"%d"), do mês $(date +"%m") do ano de $(date +"%Y")" | tee -a saudacao.log
