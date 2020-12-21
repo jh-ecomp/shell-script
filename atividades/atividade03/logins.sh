11ed118c9b97634ae876597979068d3dafc088e7
Joao_Henrique_Trixlog <joaohenrique@trixlog.com>
2020-12-21 20:34:21 -0300

    last commit

--- atividades/atividade03/logins.sh
+++ atividades/atividade03/logins.sh
@@ -4,4 +4,4 @@
 grep -Ev 'sshd' /home/compartilhado/auth.log.1
 grep -E '[(]sshd:session): session opened' /home/compartilhado/auth.log.1
 grep -E 'sshd([[:punct:]]|[[:alnum:]]|[[:space:]])*root' /home/compartilhado/auth.log.1
-grep -E 'Dec[[:space:]]*4[[:space:]]*1[8-9]:[0-9]{2}:[0-9]{2}([[:punct:]]|[[:alnum:]]|[[:space:]])*session opened' /home/compartilhado/auth.log.1
+grep -E 'Dec[[:space:]]*4[[:space:]]*((18:[0-9]{2}:[0-9]{2})|(19:00:00))([[:punct:]]|[[:alnum:]]|[[:space:]])*[(]sshd:session): session opened' /home/compartilhado/auth.log.1
