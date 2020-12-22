59f6c6b1e413f89e74b2a751d5550e4a9c5d07b8
Joao_Henrique_Trixlog <joaohenrique@trixlog.com>
2020-12-21 22:01:11 -0300

    questão 1

---
+++ atividades/atividade04/codigo.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+sed -ri 's:/bin/python:/usr/bin/python3:' /home/compartilhado/atividade04.py
+
+sed -ri 's:nota1:NOTA1:;s:nota2:NOTA2:;s:notaFinal:NOTAFINAL:' /home/compartilhado/atividade04.py
+
+sed -ri '3a \import time' /home/compartilhado/atividade04.py
+
+sed -ri '$a \   print(time.ctime())' /home/compartilhado/atividade04.py
+
