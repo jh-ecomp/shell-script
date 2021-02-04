cd886929425310d0b620d1a99a2861393682799f
Joao_Henrique_Trixlog <joaohenrique@trixlog.com>
2021-02-03 22:19:26 -0300

    segunda questão

---
+++ atividades/atividade10/contaPalavras.sh
@@ -0,0 +1,17 @@
+FILE=
+read -p "Informe o arquivo:" FILE
+
+declare -A DICT
+
+for WORD in $(cat $FILE | tr "." " "); do
+    if [  "${DICT[$WORD]}" == "" ]; then
+        DICT["$WORD"]=1
+    else
+        DICT["$WORD"]=`expr ${DICT[$WORD]} + 1`
+    fi
+
+done
+
+for WORD in ${!DICT[@]}; do
+    echo "$WORD : ${DICT[$WORD]}"
+done
